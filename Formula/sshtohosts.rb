ce3cd1f4fa9ea3fc4976cda41fe32f667fb3fb87
Miroslav Shubernetskiy <miroslav@miki725.com>
2019-05-02 11:09:32 -0400

    adding coreutils as a dep

--- Formula/sshtohosts.rb
+++ Formula/sshtohosts.rb
@@ -1,10 +1,11 @@
 class Sshtohosts < Formula
   desc "SSH to multiple hosts in different terminal windows"
   homepage "https://github.com/gamechanger/sshtohosts"
-  url "https://github.com/gamechanger/sshtohosts/archive/8921cbe1b2a37ff0c8245c9ccea0040f515f60db.tar.gz"
-  version "0.1"
-  sha256 "10bb2f58831b3c6fcd66683433abf0097c17d7a3d8c3aa781d1419f8faf2c598"
+  url "https://github.com/gamechanger/sshtohosts/archive/b8b51d1c33464f535e7428308e152bc5044e1857.tar.gz"
+  version "0.1.1"
+  sha256 "253d2d349e94c7f515559e2b41d22df1ff2650731ac0a6e7e8cde3b7f9cdd1d5"
 
+  depends_on "coreutils"
   depends_on "TomAnthony/brews/itermocil"
 
   def install
