d43aa8f702a3146ef81f379684a2266f649f0dc3
Eduardo Arenas <eduarenas80@gmail.com>
2020-06-21 10:57:43 -0400

    Add GC Awsprey

---
+++ Formula/awsprey.rb
@@ -0,0 +1,15 @@
+class Awsprey < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gamechanger/awsprey/releases/download/0.2.1/awsprey_0.2.1_darwin_amd64.tar.gz"
+  version "0.2.1"
+  sha256 "069d8537fc78ccac890113f08265c221422c1b04f4d5db2f1a5f519dc1983e90"
+
+  def install
+    bin.install "awsprey"
+  end
+
+  test do
+    
+  end
+end
